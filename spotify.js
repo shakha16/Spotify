c53f5c5d16791ce41321a94c4a8aff9726334990
Shoxrux <ganiorzikulov@gmail.com>
2023-03-19 16:00:31 +0500

    add

--- spotify.js
+++ spotify.js
@@ -17,13 +17,49 @@ arrivalStr.innerHTML = arrivalTime
 
 
 let chekIn = document.querySelector('#chekIn')
-let timeAfter1 = flightTime.slice(0,2)
+let timeAfter1 = flightTime.slice(0, 2)
 let timeAfter = timeAfter1 - 3
 
 document.onkeydown = (evt) => {
     evt = evt || window.event;
     if (evt.keyCode == 13) {
-        // if(timeAfter1 =)
-        chekIn.innerHTML = `${timeAfter}${time.innerHTML.slice(2,5)}`
+        if (timeAfter1 == '02') {
+            chekIn.innerHTML = `23${time.innerHTML.slice(2, 5)}`
+        } else if (timeAfter1 == '01') {
+            chekIn.innerHTML = `22${time.innerHTML.slice(2, 5)}`
+        } else if (timeAfter1 == '00') {
+            chekIn.innerHTML = `21${time.innerHTML.slice(2, 5)}`
+        } else {
+            chekIn.innerHTML = `${timeAfter}${time.innerHTML.slice(2, 5)}`
+        }
     }
 }
+
+
+let comp = document.querySelector('#company')
+let logo = document.querySelector('.logotype')
+
+
+
+
+document.addEventListener
+    (
+        "DOMContentLoaded",
+        function () {
+            comp.addEventListener
+                (
+                    'change',
+                    function () {
+                        let nameC = this.options[this.selectedIndex].text
+
+                        if(nameC === 'Havo yollari') {
+                            logo.src = 'https://www.uzairways.com/sites/default/files/inline-images/lg14.png'
+                        } else if (nameC === 'Flymania') {
+                            logo.src = './img/logo.jpg'
+                        } else if (nameC === 'Turkish airlines') {
+                            logo.src = 'https://companieslogo.com/img/orig/THYAO.IS-f22d40e8.png?t=1602914708'
+                        }
+                    }
+                )
+        }
+    )
